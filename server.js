5820a17b61257b1c1a35498dee61e445dff6be13
HarryMussy <207990880+HarryMussy@users.noreply.github.com>
2026-04-17 10:39:11 +0100

    CHANGED THE NAME- University Challenge to UniMinds Arena

--- server.js
+++ server.js
@@ -370,4 +370,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`University Challenge running on port ${PORT}`));
+server.listen(PORT, () => console.log(`UniMinds Arena running on port ${PORT}`));
